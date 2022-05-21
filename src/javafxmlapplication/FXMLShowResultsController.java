e84234ed341d6a8137e4d83ecc913b3d5fb2e796
mmhold <105487712+mmhold@users.noreply.github.com>
2022-05-21 21:52:36 +0200

    Make all window sizes uniform

--- src/javafxmlapplication/FXMLShowResultsController.java
+++ src/javafxmlapplication/FXMLShowResultsController.java
@@ -95,7 +95,7 @@ public class FXMLShowResultsController implements Initializable {
         FXMLHomeLoggedInController c = myLoader.<FXMLHomeLoggedInController>getController();
         
         c.initLoggedHome(primaryStage);
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, primaryStage.getWidth() - 15, primaryStage.getHeight()-38);
         //we asign new scene to current stage/window
         primaryStage.setScene(scene);
         primaryStage.setTitle("Home");
