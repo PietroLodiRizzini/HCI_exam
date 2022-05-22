a40dea7b0e05937d247d9c624b538ae7c0b96543
LucaCantagallo <74787256+LucaCantagallo@users.noreply.github.com>
2022-05-22 16:18:29 +0200

    Fixed aesthetic of first screen

--- src/javafxmlapplication/FXMLFirstScreenController.java
+++ src/javafxmlapplication/FXMLFirstScreenController.java
@@ -21,6 +21,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
