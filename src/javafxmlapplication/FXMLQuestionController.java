82f412286acd9ecdfd2ff1ef18e7c176b8ffb0bf
PietroLodiRizzini <pietro1.lodi@mail.polimi.it>
2022-05-22 19:02:15 +0200

    map drawings functionality

--- src/javafxmlapplication/FXMLQuestionController.java
+++ src/javafxmlapplication/FXMLQuestionController.java
@@ -4,6 +4,7 @@
  */
 package javafxmlapplication;
 
+import java.awt.Paint;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -16,13 +17,18 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
@@ -31,9 +37,18 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Shape;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import model.Answer;
@@ -66,16 +81,30 @@ public class FXMLQuestionController implements Initializable {
     private Slider zoom_slider;
     @FXML
     private ScrollPane map_scrollpane;
-    @FXML
-    private MenuButton map_pin;
-    @FXML
+    
     private MenuItem pin_info;
     @FXML
     private Label posicion;
-    @FXML
-    private Button markPointBtn;
-    @FXML
     private ListView<Poi> map_listview;
+    @FXML
+    private Pane mapPane;
+    @FXML
+    private ToggleButton drawPointBtn;
+
+    @FXML
+    private ToggleGroup drawToggle;
+    @FXML
+    private ToggleButton drawLineBtn;
+    @FXML
+    private ToggleButton drawArcBtn;
+    
+    private double circleCenterX;
+    private double circleCenterY;
+    
+    @FXML
+    private ToggleButton addTextBtn;
+    @FXML
+    private ColorPicker colorPicker;
 
     /**
      * Initializes the controller class.
@@ -94,7 +123,6 @@ public class FXMLQuestionController implements Initializable {
         primaryStage = stage;
         
         initProblem(problem);
-        initData();
         
         // init zoom
         
@@ -113,9 +141,8 @@ public class FXMLQuestionController implements Initializable {
         map_scrollpane.setContent(contentGroup);
     }
     
-    @FXML
     void listClicked(MouseEvent event) {
-        
+        /*
         Poi itemSelected = map_listview.getSelectionModel().getSelectedItem();
 
         // Animación del scroll hasta la posicion del item seleccionado
@@ -136,14 +163,7 @@ public class FXMLQuestionController implements Initializable {
         map_pin.setLayoutX(itemSelected.getPosition().getX());
         map_pin.setLayoutY(itemSelected.getPosition().getY());
         pin_info.setText(itemSelected.getDescription());
-        map_pin.setVisible(true);
-    }
-
-    private void initData() {
-        hm.put("2F", new Poi("2F", "Edificion del DSIC", 325, 225));
-        //hm.put("Agora", new Poi("Agora", "Agora", 600, 360));
-        map_listview.getItems().add(hm.get("2F"));
-        //map_listview.getItems().add(hm.get("Agora"));
+        map_pin.setVisible(true);*/
     }
     
     /**
@@ -222,21 +242,144 @@ public class FXMLQuestionController implements Initializable {
     private void muestraPosicion(MouseEvent event) {
     }
 
-    @FXML
-    private void markPointClick(ActionEvent event) {
-    }
 
+    /**
+     * Allows to draw a point if the point draw toggle button is selected.
+     * @param event 
+     */
     @FXML
-    private void paneMouseClick(MouseEvent event) {
+    private void paneMouseClick(MouseEvent event) {        
+        System.out.println("Pane click");
+        Toggle selectedToggle = drawToggle.getSelectedToggle();
+        
         double x = event.getX();
         double y = event.getY();
         
-        Poi p = new Poi();
-        p.setPosition(new Point2D(x, y));
-        p.setCode("11");
-        hm.put("new", p);
-        System.out.println("x: " + x + "y: " + y);
-        map_listview.getItems().add(hm.get("new"));
+        if(selectedToggle == drawPointBtn) {
+            System.out.println("draw point");
+            Circle c = new Circle();
+
+            c.setRadius(5.0);
+            c.setCenterX(x);
+            c.setCenterY(y);
+            c.setStroke(Color.BLUE);
+            c.setStrokeWidth(0.0);
+            c.setOnMouseClicked(this::onShapeClick);
+            mapPane.getChildren().add(c);
+            drawToggle.selectToggle(null);
+        }
+    }
+    
+    /**
+     * Allows to start drawing lines, circles and adding text.
+     * @param event 
+     */
+    @FXML
+    private void paneMousePressed(MouseEvent event) {
+        Toggle selectedToggle = drawToggle.getSelectedToggle();
+        
+        double x = event.getX();
+        double y = event.getY();
+        
+        if(selectedToggle == drawLineBtn) {
+            
+            Line linePainting = new Line(x, y, x, y);
+            linePainting.setStrokeWidth(5.0);
+            zoomGroup.getChildren().add(linePainting);
+            
+        } else if(selectedToggle == drawArcBtn) {
+            
+            Circle circle = new Circle();
+            circle.setCenterX(x);
+            circle.setCenterY(y);
+            circle.setFill(Color.rgb(0, 0, 0, 0.0));
+            circle.setStroke(Color.rgb(0, 0, 0, 1.0));
+            circleCenterX = x;
+            circleCenterY = y;
+            circle.setStrokeWidth(5.0);
+            zoomGroup.getChildren().add(circle);
+            
+        } else if(selectedToggle == addTextBtn) {
+            
+            TextField textField = new TextField();
+            zoomGroup.getChildren().add(textField);
+            textField.setLayoutX(event.getX());
+            textField.setLayoutY(event.getY());
+            textField.requestFocus();
+            
+            // When the user is done editing the text, TextField gets converted to a Text:
+            textField.setOnAction(e -> {
+                Text label = new Text(textField.getText());
+                label.setX(textField.getLayoutX());
+                label.setY(textField.getLayoutY());
+                label.setStyle("-fx-font-family: Gafatar;-fx-font-size: 20;");
+                label.setOnMouseClicked(this::onShapeClick);
+                zoomGroup.getChildren().add(label);
+                zoomGroup.getChildren().remove(textField);
+                e.consume();
+            });
+            
+        }
+    }
+    
+    /**
+     * Allows color change on shape click.
+     */
+    private void onShapeClick(Event e) {
+        // give priority to drawing functionalities
+        if(drawToggle.getSelectedToggle() != null)
+            return;
+        
+        Object source = e.getSource();
+        Parent p = (Parent) source;
+        
+        if(source instanceof Shape) {
+            Shape s = (Shape) source;
+            s.setStroke(colorPicker.getValue());
+            s.setFill(colorPicker.getValue());
+        } else if(source instanceof Text) {
+            Text t = (Text) source;
+            t.setStroke(colorPicker.getValue());
+            t.setFill(colorPicker.getValue());
+        }
+    }
+    
+
+    @FXML
+    private void paneMouseDragged(MouseEvent event) {
+        Toggle selectedToggle = drawToggle.getSelectedToggle();
+        
+        double x = event.getX();
+        double y = event.getY();
+        
+        // element that is being painted
+        Node p = zoomGroup.getChildren().get(zoomGroup.getChildren().size() - 1);
+        
+        if(selectedToggle == drawLineBtn) {
+            Line linePainting = (Line) p;
+            linePainting.setEndX(x);
+            linePainting.setEndY(y);
+            linePainting.setOnMouseClicked(this::onShapeClick);
+            event.consume();
+        } else if(selectedToggle == drawArcBtn) {
+            Circle circle = (Circle) p;
+            double newRadius = this.calculateDistanceBetweenPoints(circleCenterX, circleCenterY, x, y);
+            circle.setOnMouseClicked(this::onShapeClick);
+            circle.setRadius(newRadius);
+        }
+    }
+    
+    private double calculateDistanceBetweenPoints(double x1, double y1, double x2, double y2) {       
+        return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+    }
+
+    @FXML
+    private void paneMouseReleased(MouseEvent event) {
+        Toggle selectedToggle = drawToggle.getSelectedToggle();
+        
+        if(selectedToggle != drawPointBtn)
+        drawToggle.selectToggle(null);
     }
     
 }
+    
